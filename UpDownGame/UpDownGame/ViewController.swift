99baf01b0a033b58bf9e0273f33ebc222d9c191a
Daye Yoon <ydyelin@gmail.com>
2021-03-10 22:15:47 +0900

    210310

---
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by Daye on 2021/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var slider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func sliderValueChanged(_ sender: UISlider){
+        print(sender.value)
+        
+    }
+
+    @IBAction func touchUpHitButton(_ sender: UIButton){
+        print(slider.value)
+    }
+
+    @IBAction func touchUpResetButton(_ sender: UIButton){
+        
+        let message = "touch up reset button"
+        let alert = UIAlertController(title:  "Alert message", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Confirm", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+        print("touch up reset button")
+    }
+}
+
